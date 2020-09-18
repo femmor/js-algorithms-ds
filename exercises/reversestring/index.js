0866981b77d4133650f6f8bb044035e648d250c9
Emmanuel Egomson <fegomson@gmail.com>
2020-09-18 09:45:22 -0500

    Solution 1 - reversestring algorithm

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // Solution 1
+    // convert the given str to an array
+    // use the reverse helper method to reverse the array
+    // join the array back into a str with the join method
+    return str.split("").reverse().join("");
+}
 
 module.exports = reverse;
