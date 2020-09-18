c058b8f23bde00b7905e74831b788c8694d0d184
Emmanuel Egomson <fegomson@gmail.com>
2020-09-18 09:50:17 -0500

    Solution 2 - reversestring algorithm

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,11 +7,22 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    // Solution 1
-    // convert the given str to an array
-    // use the reverse helper method to reverse the array
-    // join the array back into a str with the join method
-    return str.split("").reverse().join("");
+    // // Solution 1
+    // // convert the given str to an array
+    // // use the reverse helper method to reverse the array
+    // // join the array back into a str with the join method
+    // return str.split("").reverse().join("");
+
+    // Solution 2
+    // Using forOf loop
+    // create a new empty string
+    let reversed = ""
+    // for each char in the the given str, add the char to the empty reversed string
+    for (const char of str) {
+        reversed = char + reversed
+    }
+    // return the reversed string
+    return reversed
 }
 
 module.exports = reverse;
