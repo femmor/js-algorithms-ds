4906fc15c04eb35da460b0c0b5b65bf12e1f388c
Emmanuel Egomson <fegomson@gmail.com>
2020-09-22 19:46:23 -0500

    Solution - fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,25 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        // is the number a multiple of 3 and 5?
+        if (i % 15 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz")
+        } 
+        // is the number a multiple of 3?
+        else if (i % 3 === 0) {
+            console.log("fizz")
+        }
+        // is the number a multiple of 5?
+        else if (i % 5 === 0) {
+            console.log("buzz")
+        }
+        // else just print the number
+        else {
+            console.log(i)
+        }
+    }
+}
 
 module.exports = fizzBuzz;
